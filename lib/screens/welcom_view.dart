8fdcd7416178ddd00619a74c683389d776bf1ee4
user <dissanayakepiumi75@gmail.com>
2024-01-19 00:10:05 +0530

    my first commit

---
+++ lib/screens/welcom_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class WelcomView extends StatefulWidget{
+  @override
+  _WelcomViewState createState() => _WelcomViewState();
+}
+
+class _WelcomViewState extends State<WelcomView> {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body:Container(
+
+      ),
+    );
+  }
+}
